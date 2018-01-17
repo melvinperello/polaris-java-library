931d3da7bd91de0c495a54ef58f319652d2ae383
joemardc98 <dlcrzjoemz12@gmail.com>
2018-01-17 22:36:57 +0800

    added sample jchecker

--- src/afterschoolcreatives/polaris/java/util/JStringChecker.java
+++ src/afterschoolcreatives/polaris/java/util/JStringChecker.java
@@ -3,8 +3,25 @@ package afterschoolcreatives.polaris.java.util;
 /**
  * Joemar String Checker class.
  *
- * @author YOUR NAME
+ * @author Joemar
  */
 public class JStringChecker {
-
+    
+    /**
+     * checks the given string if all given char are letters
+     * @param str
+     * @return true if all given char in string are letters
+     */
+    public static boolean areAlpha(String str) {
+        return str.matches("\\p{L}+");
+    }
+    
+    /**
+     * checks the given string if all given char are numbers
+     * @param str
+     * @return true if all given char in string are numbers
+     */
+    public static boolean areNumeric(String str) {
+        return str.matches("\\p{N}+");
+    }
 }
