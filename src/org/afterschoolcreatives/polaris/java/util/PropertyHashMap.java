f8a1c95bee0f173974b32eed5153ccd57681b77f
jhmvin <izumiiii.10@gmail.com>
2018-01-23 04:15:26 +0800

    added license notice

--- src/org/afterschoolcreatives/polaris/java/util/PropertyHashMap.java
+++ src/org/afterschoolcreatives/polaris/java/util/PropertyHashMap.java
@@ -1,9 +1,31 @@
+/**
+ *
+ * Polaris Java Library - Afterschool Creatives "Captivating Creativity"
+ *
+ * Copyright 2018 Jhon Melvin Perello
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
 package org.afterschoolcreatives.polaris.java.util;
 
 import java.util.HashMap;
-import org.afterschoolcreatives.polaris.java.io.FileReading;
-import org.afterschoolcreatives.polaris.java.io.FileWriting;
-import java.io.IOException;
 
 /**
  * This class acts as a normal HashMap but has extended features to read
@@ -12,32 +34,11 @@ import java.io.IOException;
  *
  * @author Jhon Melvin
  */
-public class PropertyHashMap extends HashMap<String, String>
-        implements FileReading, FileWriting {
+public class PropertyHashMap extends HashMap<String, String> {
 
     /**
      * Serialization.
      */
     private static final long serialVersionUID = 1L;
-
-    /**
-     * Read contents of a property file.
-     *
-     * @throws IOException
-     */
-    @Override
-    public void fileRead() throws IOException {
-
-    }
-
-    /**
-     * Write contents to a property file.
-     *
-     * @throws IOException
-     */
-    @Override
-    public void fileWrite() throws IOException {
-
-    }
 
 }
